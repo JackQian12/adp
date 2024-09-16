897c9dce57f729fe001fbb1464d5bfd765ef5df9
JackQQQQQQ <35983644+JackQQQQQQ@users.noreply.github.com>
2024-09-16 17:33:10 +0800

    更新了新增方法

--- app.py
+++ app.py
@@ -1,69 +1,61 @@
-from flask import Flask, render_template, request
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_database.db'
 db = SQLAlchemy(app)
 
-
-def check_connection():
-    try:
-        engine = db.engine
-        with engine.connect() as connection:
-            return True
-    except Exception as e:
-        return False
-
 # 定义一个简单的数据模型
-class User(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     age = db.Column(db.Integer)
 
-# 定义一个理赔台账的数据模型
-class Client(db.Model):
-    id = db.Column(db.String(80), primary_key=True)
+class Clients(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    mobile = db.Column(db.String(200), nullable=False)
-    descript = db.Column(db.String(2000), nullable=False)
+    mobile = db.Column(db.String(80), nullable=False)
+    descript =db.Column(db.String(80), nullable=False)
+    idno = db.Column(db.String(80), nullable=False)
+    
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    if check_connection():
-        print("Database connection is successful.")
-    else:
-        print("Database connection failed.")
-    app.run(debug=True)
+# 创建数据库表（如果不存在）
+with app.app_context():
+    db.create_all()
 
 # API 路由，返回所有用户
-@app.route('/getusers', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def get_users():
-    users = User.query.all()
-    return jsonify([{'id': user.id, 'name': user.name} for user in users])
+    users = Users.query.all()
+    return jsonify([{'id': user.id, 'name': user.name,'age':user.age} for user in users])
 
-@app.route('/addusers', methods=['POST'])
-def add_user():
+@app.route('/clients', methods=['GET'])
+def get_clients():
+    clients = Clients.query.all()
+    return jsonify([{'idno': client.idno, 'name': client.name,'mobile':client.mobile,'descript':client.descript} for client in clients])
+
+@app.route('/clients/<mobile>', methods=['GET'])
+def search_clients_by_mobile(mobile):
+    client = Clients.query.filter_by(mobile=mobile).first()
+    if client:
+        return jsonify({'id': client.id, 'name': client.name, 'mobile': client.mobile, 'descript': client.descript})
+    else:
+        return jsonify({'message': 'Client not found'})
+
+@app.route('/clients', methods=['POST'])
+def add_client():
     data = request.get_json()
-    new_user = User(name=data['name'], age=data['age'])
-    db.session.add(new_user)
+    name = data.get('name')
+    mobile = data.get('mobile')
+    descript = data.get('descript')
+    idno = data.get('idno')
+    if not name or not mobile or not descript:
+        return jsonify({'message': 'Missing required fields'}), 400
+    client = Clients(idno=idno,name=name, mobile=mobile, descript=descript)
+    db.session.add(client)
     db.session.commit()
-    return jsonify({'message': 'User added successfully'}), 201
+    return jsonify({'message': 'Client added successfully'}), 201
 
-# API 路由，返回所有报案人
-@app.route('/getclients', methods=['GET'])
-def get_Clients():
-    Clients = Client.query.all()
-    return jsonify([{'id': client.id, 'name': client.name,'mobile':client.mobile,'descript':client.descript} for client in Clients])
 
-@app.route('/addclients', methods=['POST'])
-def add_Client():
-    data=request.get_json()
-    new_client=Client(name=data['name'],mobile=data['mobile'],descript=data['descript'])
-    db.session.add(new_client)
-    db.session.commit()
-    return jsonify({'message':'new client added'})
-
+if __name__ == '__main__':
+    app.run(debug=True)
